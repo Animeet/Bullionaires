f201467a9d12dd07abf2db0179afba6625579855
Zachary Barnes <zachhsavage@gmail.com>
2023-09-18 09:21:09 -0400

    Added a quotes section break

--- assets/js/script.js
+++ assets/js/script.js
@@ -51,3 +51,72 @@ var widgetConfig = {
   // Find the tradingview-widget container element and append the script to it
   var tradingViewContainer = document.getElementById("tradingview-widget");
   tradingViewContainer.appendChild(script);
+
+
+  var slideIndex = 1;
+showSlides(slideIndex);
+
+// Automatically transition to the next slide every 2 seconds (2000 milliseconds)
+var slideInterval = setInterval(function() {
+  plusSlides(1);
+}, 3000);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {
+      slideIndex = 1;
+    }
+    if (n < 1) {
+      slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+      slides[i].classList.remove("fade", "fade-in", "fade-out"); // Remove the fade classes
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+  
+    // Apply fade-out class to the current slide after a delay
+    setTimeout(function () {
+      var currentSlide = document.getElementsByClassName("mySlides")[slideIndex - 1];
+      if (currentSlide) {
+        currentSlide.classList.add("fade-out");
+      }
+    }, 100); // Adjust the delay as needed
+  
+    // Apply fade-in class to the new slide after a short delay
+    setTimeout(function () {
+      var newSlide = document.getElementsByClassName("mySlides")[slideIndex - 1];
+      if (newSlide) {
+        newSlide.classList.add("fade-in");
+      }
+    }, 200); // Adjust the delay as needed
+  
+    dots[slideIndex - 1].className += " active";
+  }
+
+// Stop the automatic slide transition on mouse hover
+var slideshowContainer = document.querySelector(".slideshow-container");
+
+slideshowContainer.addEventListener("mouseenter", function () {
+  clearInterval(slideInterval);
+});
+
+// Resume automatic slide transition on mouse leave
+slideshowContainer.addEventListener("mouseleave", function () {
+  slideInterval = setInterval(function () {
+    plusSlides(1);
+  }, 3000);
+});
